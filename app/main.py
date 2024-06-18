77a4b7453c2f650fe3717a804055d0dfa92bb196
LuizGPinheiro <lg.pinheirodev@gmail.com>
2024-06-18 20:29:19 -0300

    Creating a function to return in the format of a list of objects.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import os
 import fitz  # PyMuPDF
 import re
+from datetime import datetime
 
 def pdf_to_text(pdf_path):
     # Abre o arquivo PDF
@@ -15,67 +16,50 @@ def pdf_to_text(pdf_path):
     return text
 
 def extract_values_from_text(text, patterns):
-    results = {}
+    results = []
     for key, (pattern, unit) in patterns.items():
         match = re.search(pattern, text, re.DOTALL)
         if match:
-            results[key] = {
-                "value": match.group(1),
-                "unit": unit
-            }
+            results.append({
+                "tipo": key,
+                "valor": f"{match.group(1)} {unit}"
+            })
     return results
 
 def extract_exam_date(text):
     pattern_date = r'Atendimento\s*:\s*(\d{2}/\d{2}/\d{4})'
     match = re.search(pattern_date, text)
     if match:
-        return match.group(1)
+        return datetime.strptime(match.group(1), '%d/%m/%Y')
     return None
 
 def extract_blood_values(pdf_path):
     # Define os padrões de expressões regulares e unidades correspondentes
     patterns = {
-        "hemoglobin": (r'HEMOGLOBINA(?:\s*?:\s*?|.*?RESULTADO:\s*)(\d+,\d+|\d+\.\d+|\d+)', "g/dL"),
-        "hematocrit": (r'HEMATÓCRITO(?:\s*?:\s*?|.*?RESULTADO:\s*)(\d+,\d+|\d+\.\d+|\d+)%?', "%"),
-        "eritrocitos": (r'HEMÁCIAS(?:\s*?:\s*?|.*?RESULTADO:\s*)(\d+,\d+|\d+\.\d+|\d+)', "milhões/mm3"),
-        "hba1c": (r'HEMOGLOBINA GLICADA - HbA1c\s*.*?RESULTADO:\s*(\d+,\d+|\d+\.\d+)\s*%', "%"),
-        "ast": (r'TRANSAMINASE OXALACÉTICA TGO \(AST\).*?RESULTADO:\s*([\d,\.]+)\s*U/L', "U/L"),
-        "alt": (r'TRANSAMINASE PIRÚVICA TGP \(ALT\).*?RESULTADO:\s*([\d,\.]+)\s*U/L', "U/L"),
-        "ureia": (r'UREIA\s*.*?RESULTADO:\s*(\d+|\d+,\d+|\d+\.\d+)\s*mg/dL', "mg/dL"),
-        "creatinina": (r'CREATININA\s*.*?RESULTADO:\s*(\d+,\d+|\d+\.\d+)\s*mg/dL', "mg/dL")
+        "Hemoglobina": (r'HEMOGLOBINA(?:\s*?:\s*?|.*?RESULTADO:\s*)(\d+,\d+|\d+\.\d+|\d+)', "g/dL"),
+        "Hematócrito": (r'HEMATÓCRITO(?:\s*?:\s*?|.*?RESULTADO:\s*)(\d+,\d+|\d+\.\d+|\d+)%?', "%"),
+        "Eritrócitos (Hemácias)": (r'HEMÁCIAS(?:\s*?:\s*?|.*?RESULTADO:\s*)(\d+,\d+|\d+\.\d+|\d+)', "milhões/mm3"),
+        "Hemoglobina Glicada (HbA1c)": (r'HEMOGLOBINA GLICADA - HbA1c\s*.*?RESULTADO:\s*(\d+,\d+|\d+\.\d+)\s*%', "%"),
+        "AST (TGO)": (r'TRANSAMINASE OXALACÉTICA TGO \(AST\).*?RESULTADO:\s*([\d,\.]+)\s*U/L', "U/L"),
+        "ALT (TGP)": (r'TRANSAMINASE PIRÚVICA TGP \(ALT\).*?RESULTADO:\s*([\d,\.]+)\s*U/L', "U/L"),
+        "Ureia": (r'UREIA\s*.*?RESULTADO:\s*(\d+|\d+,\d+|\d+\.\d+)\s*mg/dL', "mg/dL"),
+        "Creatinina": (r'CREATININA\s*.*?RESULTADO:\s*(\d+,\d+|\d+\.\d+)\s*mg/dL', "mg/dL")
     }
     
     # Converte o PDF para texto
     pdf_text = pdf_to_text(pdf_path)
     
-    # Extrai os valores
-    blood_values = extract_values_from_text(pdf_text, patterns)
-    
     # Extrai a data do exame
     exam_date = extract_exam_date(pdf_text)
     
-    return blood_values, exam_date
+    # Extrai os valores
+    blood_values = extract_values_from_text(pdf_text, patterns)
+    
+    # Adiciona a data do exame a cada valor
+    for value in blood_values:
+        value["data_exame"] = exam_date
 
-def print_blood_values(blood_values, exam_date):
-    # Define os campos e a ordem de impressão
-    fields = [
-        ("Hemoglobina", "hemoglobin"),
-        ("Hematócrito", "hematocrit"),
-        ("Eritrócitos (Hemácias)", "eritrocitos"),
-        ("Hemoglobina Glicada (HbA1c)", "hba1c"),
-        ("AST (TGO)", "ast"),
-        ("ALT (TGP)", "alt"),
-        ("Ureia", "ureia"),
-        ("Creatinina", "creatinina")
-    ]
-    
-    # Imprime a data do exame
-    print(f"Data do Exame: {exam_date}")
-    
-    for label, key in fields:
-        value = blood_values.get(key, {}).get("value", "")
-        unit = blood_values.get(key, {}).get("unit", "")
-        print(f"Valor {label}: {value} {unit}")
+    return blood_values
 
 # Obtém o diretório do script atual
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -84,7 +68,8 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 pdf_path = os.path.join(script_dir, 'hemograma.pdf')  # Caminho absoluto para o arquivo PDF
 
 # Extrai os valores do PDF
-blood_values, exam_date = extract_blood_values(pdf_path)
+blood_values = extract_blood_values(pdf_path)
 
-# Imprime os valores extraídos com unidades
-print_blood_values(blood_values, exam_date)
+# Imprime os valores extraídos
+for value in blood_values:
+    print(value)
